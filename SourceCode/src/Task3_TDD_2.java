599f74303ef738b738282bdec4618d1ddcacb435
Ellie Nguyen <s1898186@dumoulin.inf.ed.ac.uk>
2019-03-27 22:54:40 +0000

    Second commit

---
+++ SourceCode/src/Task3_TDD_2.java
@@ -0,0 +1,287 @@
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import st.Parser;
+
+public class Task3_TDD_2 {
+
+	private Parser parser;
+	private List<Integer> expected;
+
+	@Before
+	public void set_up() {
+		parser = new Parser();
+		expected = asList();
+	}
+
+	@Test
+	public void test_parse_var_1() {
+		expected = asList(1);
+		parser.add("a", "a", Parser.STRING);
+		parser.parse("--a=1");
+		assertEquals(parser.getIntegerList("a"), expected);
+
+		parser.add("a", "a", Parser.BOOLEAN);
+		parser.parse("--a=1");
+		assertEquals(parser.getIntegerList("a"), expected);
+
+		parser.parse("--a=0");
+		assertEquals(parser.getIntegerList("a"), asList(0));
+
+		parser.add("a", "a", Parser.CHAR);
+		parser.parse("--a=1");
+		assertEquals(parser.getIntegerList("a"), expected);
+
+		parser.add("a", "a", Parser.INTEGER);
+		parser.parse("--a=1");
+		assertEquals(parser.getIntegerList("a"), expected);
+
+		parser.parse("--a=1214312");
+		expected = asList(1214312);
+		assertEquals(parser.getIntegerList("a"), expected);
+
+		parser.parse("--a=9999999999");
+		expected = asList();
+		assertEquals(parser.getIntegerList("a"), expected);
+	}
+
+	@Test
+	public void test_parse_var_2() {
+		parser.add("a", "a", Parser.STRING);
+		parser.parse("--a=abc");
+		assertEquals(parser.getIntegerList("a"), expected);
+
+		parser.add("a", "a", Parser.BOOLEAN);
+		parser.parse("--a=true");
+		assertEquals(parser.getIntegerList("a"), asList(1));
+
+		parser.add("a", "a", Parser.CHAR);
+		parser.parse("--a=a");
+		assertEquals(parser.getIntegerList("a"), expected);
+
+		expected = asList(1);
+		parser.add("a", "a", Parser.STRING);
+		parser.parse("--a=ab1c");
+		assertEquals(parser.getIntegerList("a"), expected);
+	}
+
+	@Test
+	public void test_parse_var_3() {
+		parser.add("a", "a", Parser.STRING);
+		parser.parse("--a=-1");
+		expected = asList(-1);
+		assertEquals(parser.getIntegerList("a"), expected);
+
+		parser.parse("--a=-1234");
+		expected = asList(-1234);
+		assertEquals(parser.getIntegerList("a"), expected);
+
+		parser.parse("--a=-99999999999");
+		expected = asList();
+		assertEquals(parser.getIntegerList("a"), expected);
+	}
+
+	@Test
+	public void test_parse_op_1() {
+		expected = asList(1, 2);
+		parser.add("a", "a", Parser.STRING);
+		parser.parse("--a=1,2");
+		assertEquals(parser.getIntegerList("a"), expected);
+
+		parser.parse("--a=1abc2");
+		assertEquals(parser.getIntegerList("a"), expected);
+
+		expected = asList(1, 2, 3);
+		parser.parse("--a=1,2,3");
+		assertEquals(parser.getIntegerList("a"), expected);
+
+		expected = asList(111, 222, 333);
+		parser.parse("--a=111,222,333");
+		assertEquals(parser.getIntegerList("a"), expected);
+
+		parser.parse("--a=,111,222,333,");
+		assertEquals(parser.getIntegerList("a"), expected);
+
+		parser.parse("--a=((111,222,333))");
+		assertEquals(parser.getIntegerList("a"), expected);
+	}
+
+	@Test
+	public void test_parse_op_2() {
+		expected = asList(-1, 1);
+		parser.add("a", "a", Parser.STRING);
+		parser.parse("--a=1,-1");
+		assertEquals(parser.getIntegerList("a"), expected);
+
+		parser.parse("--a=1abc-1");
+		assertEquals(parser.getIntegerList("a"), expected);
+
+		expected = asList(-2, 1, 3);
+		parser.parse("--a=1,-2,3");
+		assertEquals(parser.getIntegerList("a"), expected);
+
+		expected = asList(-222, 111, 333);
+		parser.parse("--a=((111,-222,333))");
+		assertEquals(parser.getIntegerList("a"), expected);
+	}
+
+	@Test
+	public void test_parse_op_3() {
+		expected = asList(1, 2);
+		parser.add("a", "a", Parser.STRING);
+		parser.parse("--a=1-2");
+		assertEquals(parser.getIntegerList("a"), expected);
+
+		parser.parse("--a=2-1");
+		assertEquals(parser.getIntegerList("a"), expected);
+
+		parser.parse("--a=4-1");
+		expected = asList(1, 2, 3, 4);
+		assertEquals(parser.getIntegerList("a"), expected);
+
+		parser.parse("--a=-3--1");
+		expected = asList(-3, -2, -1);
+		assertEquals(parser.getIntegerList("a"), expected);
+
+		parser.parse("--a=-1--3");
+		assertEquals(parser.getIntegerList("a"), expected);
+
+		parser.parse("--a=-3-1");
+		expected = asList(-3, -2, -1, 0, 1);
+		assertEquals(parser.getIntegerList("a"), expected);
+
+		parser.parse("--a=1--3");
+		assertEquals(parser.getIntegerList("a"), expected);
+
+	}
+
+	@Test
+	public void test_parse_op_4() {
+		parser.add("a", "a", Parser.STRING);
+
+		expected = asList(1, 2, 3);
+		parser.parse("--a=1-2,3");
+		assertEquals(parser.getIntegerList("a"), expected);
+
+		expected = asList(0, 1, 2);
+		parser.parse("--a=1-2,0");
+		assertEquals(parser.getIntegerList("a"), expected);
+
+		parser.parse("--a=-3-1,1-2");
+		expected = asList(-3, -2, -1, 0, 1, 1, 2);
+		parser.getIntegerList("a");
+		assertEquals(parser.getIntegerList("a"), expected);
+
+		parser.parse("--a=-3-1!!!1-2");
+		assertEquals(parser.getIntegerList("a"), expected);
+
+	}
+
+	@Test
+	public void test_parse_op_5() {
+		parser.add("a", "a", Parser.STRING);
+		expected = asList(1, 1, 1);
+		parser.parse("--a=1=1=1");
+		assertEquals(parser.getIntegerList("a"), expected);
+
+		expected = asList(0, 0, 1, 1, 2, 2);
+		parser.parse("--a=0-2,0-2");
+		assertEquals(parser.getIntegerList("a"), expected);
+
+		expected = asList(-1, -1, -1);
+		parser.parse("--a=-1=-1=-1");
+		assertEquals(parser.getIntegerList("a"), expected);
+	}
+
+	@Test
+	public void test_parse_op_6() {
+		parser.add("a", "a", Parser.STRING);
+		parser.parse("--a=3,2,1");
+		assertEquals(parser.getIntegerList("a"), asList(1, 2, 3));
+
+		parser.parse("--a=2,3,1");
+		assertEquals(parser.getIntegerList("a"), asList(1, 2, 3));
+
+		parser.parse("--a=-1,-2,-3");
+		assertEquals(parser.getIntegerList("a"), asList(-3, -2, -1));
+
+		parser.parse("--a=-2,-3,-1");
+		assertEquals(parser.getIntegerList("a"), asList(-3, -2, -1));
+	}
+
+	@Test
+	public void test_parse_op_7() {
+		parser.add("a", "a", Parser.STRING);
+		parser.parse("--a=1,2");
+		assertEquals(parser.getIntegerList("a"), asList(1, 2));
+		assertEquals(parser.getInteger("a"), 0);
+		assertEquals(parser.getString("a"), "1,2");
+		assertEquals(parser.getChar("a"), '1');
+		assertEquals(parser.getBoolean("a"), true);
+
+		parser.parse("--a=111,222,333");
+		assertEquals(parser.getIntegerList("a"), asList(111, 222, 333));
+		assertEquals(parser.getInteger("a"), 0);
+		assertEquals(parser.getString("a"), "111,222,333");
+		assertEquals(parser.getChar("a"), '1');
+		assertEquals(parser.getBoolean("a"), true);
+
+		parser.parse("--a=-3-1,1-2");
+		assertEquals(parser.getIntegerList("a"), asList(-3, -2, -1, 0, 1, 1, 2));
+		assertEquals(parser.getInteger("a"), 0);
+		assertEquals(parser.getString("a"), "-3-1,1-2");
+		assertEquals(parser.getChar("a"), '-');
+		assertEquals(parser.getBoolean("a"), true);
+	}
+
+	@Test
+	public void test_parse_pre_1() {
+		parser.add("a", "a", Parser.STRING);
+		parser.parse("--a 1");
+		assertEquals(parser.getIntegerList("a"), asList(1));
+
+		parser.add("a", "ab", Parser.CHAR);
+		parser.parse("-ab 1");
+		assertEquals(parser.getIntegerList("a"), asList(1));
+
+		parser.add("a", "a", Parser.STRING);
+		parser.add("b", "b", Parser.STRING);
+		parser.parse("-a=1 -b=1");
+		assertEquals(parser.getIntegerList("a"), asList(1));
+		assertEquals(parser.getIntegerList("a"), asList(1));
+	}
+
+	@Test
+	public void test_parse_pre_2() {
+		parser.add("a", "a", Parser.STRING);
+		parser.parse("--a='1'");
+		assertEquals(parser.getIntegerList("a"), asList(1));
+
+		parser.parse("-a=\"1\"");
+		assertEquals(parser.getIntegerList("a"), asList(1));
+
+		String test = "--a='\"1\"2'";
+		parser.parse(test);
+		assertEquals(parser.getIntegerList("a"), asList(1, 2));
+	}
+
+	@Test
+	public void test_parse_pre_3() {
+		parser.add("a", "a", Parser.STRING);
+		parser.parse("--a=-1-");
+		assertEquals(parser.getIntegerList("a"), asList());
+
+		parser.parse("--a=1,2-");
+		assertEquals(parser.getIntegerList("a"), asList());
+
+		parser.parse("--a=1,2-ghs");
+		assertEquals(parser.getIntegerList("a"), asList());
+
+	}
+
+}
--- SourceCode/src/st/Parser.java
+++ SourceCode/src/st/Parser.java
@@ -1,23 +1,28 @@
 package st;
 
+import static java.util.Arrays.asList;
+
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Parser {
 	public static final int INTEGER = 1;
 	public static final int BOOLEAN = 2;
 	public static final int STRING = 3;
 	public static final int CHAR = 4;
-	
+
 	private OptionMap optionMap;
-	
+
 	public Parser() {
 		optionMap = new OptionMap();
 	}
-	
+
 	public void add(String option_name, String shortcut, int value_type) {
 		optionMap.store(option_name, shortcut, value_type);
 	}
-	
+
 	public void add(String option_name, int value_type) {
 		optionMap.store(option_name, "", value_type);
 	}
@@ -31,13 +36,13 @@ public class Parser {
 			try {
 				result = Integer.parseInt(value);
 			} catch (Exception e) {
-		        try {
-		            new BigInteger(value);
-		        } catch (Exception e1) {
-		            result = 0;
-		        }
-		        result = 0;
-		    }
+				try {
+					new BigInteger(value);
+				} catch (Exception e1) {
+					result = 0;
+				}
+				result = 0;
+			}
 			break;
 		case BOOLEAN:
 			if (getBoolean(option) == false) {
@@ -47,25 +52,26 @@ public class Parser {
 			}
 			break;
 		case STRING:
-		    int power = 1;
-		    result = 0;
-		    char c;
-		    for (int i = value.length() - 1; i >= 0; --i){
-		        c = value.charAt(i);
-		        if (!Character.isDigit(c)) return 0;
-		        result = result + power * (c - '0');
-		        power *= 10;
-		    }
-		    break;
+			int power = 1;
+			result = 0;
+			char c;
+			for (int i = value.length() - 1; i >= 0; --i) {
+				c = value.charAt(i);
+				if (!Character.isDigit(c))
+					return 0;
+				result = result + power * (c - '0');
+				power *= 10;
+			}
+			break;
 		case CHAR:
-			result = (int)getChar(option);
+			result = (int) getChar(option);
 			break;
 		default:
 			result = 0;
 		}
 		return result;
 	}
-	
+
 	public boolean getBoolean(String option) {
 		String value = getString(option);
 		boolean result;
@@ -76,12 +82,12 @@ public class Parser {
 		}
 		return result;
 	}
-	
+
 	public String getString(String option) {
 		String result = optionMap.getValue(option);
 		return result;
 	}
-	
+
 	public char getChar(String option) {
 		String value = getString(option);
 		char result;
@@ -92,7 +98,124 @@ public class Parser {
 		}
 		return result;
 	}
-	
+
+	public List<Integer> getIntegerList(String option) {
+		String value = getString(option);
+		int type = getType(option);
+		List<Integer> result = new ArrayList<Integer>();
+		switch (type) {
+		case INTEGER:
+			try {
+				result.add(Integer.parseInt(value));
+			} catch (Exception e) {
+				try {
+					new BigInteger(value);
+				} catch (Exception e1) {
+
+				}
+			}
+			break;
+		case BOOLEAN:
+			if (getBoolean(option) == false) {
+				result = asList(0);
+			} else {
+				result = asList(1);
+			}
+			break;
+		case STRING:
+			try {
+				result.add(Integer.parseInt(value));
+			} catch (Exception e) {
+				boolean noNumber = true;
+				for (char c : value.toCharArray()) {
+					if (Character.isDigit(c)) {
+						noNumber = false;
+					}
+				}
+				if (noNumber) {
+					break;
+				}
+				String[] splited = value.split("[^\\d-]");
+				for (String s : splited) {
+					if (s.length() != 0) {
+						if (s.contains("-")) {
+							if (s.charAt(s.length() - 1) == '-') {
+								result = asList();
+								break;
+							}
+
+							int count = 0;
+							for (char c : s.toCharArray()) {
+								if (c == '-') {
+									count++;
+								}
+							}
+
+							switch (count) {
+							case (1):
+								if (s.charAt(0) == '-') {
+									try {
+										result.add(Integer.parseInt(s));
+									} catch (NumberFormatException error) {
+
+									}
+								} else {
+									String[] intervalSplit = s.split("-");
+									int first = Integer.parseInt(intervalSplit[0]);
+									int second = Integer.parseInt(intervalSplit[1]);
+									for (int i = Math.min(first, second); i <= Math.max(first, second); i++) {
+										result.add(i);
+									}
+								}
+								break;
+							case (2):
+								int first = 0;
+								int second = 0;
+
+								if (s.charAt(0) == '-') {
+									s = s.substring(1);
+									String[] intervalSplit = s.split("-");
+									first = -1 * Integer.parseInt(intervalSplit[0]);
+									second = Integer.parseInt(intervalSplit[1]);
+								} else {
+									String[] intervalSplit = s.split("-");
+									first = Integer.parseInt(intervalSplit[0]);
+									second = -1 * Integer.parseInt(intervalSplit[2]);
+								}
+								for (int i = Math.min(first, second); i <= Math.max(first, second); i++) {
+									result.add(i);
+								}
+								break;
+
+							case (3):
+								String[] intervalSplit = s.split("-");
+								first = -1 * Integer.parseInt(intervalSplit[1]);
+								second = -1 * Integer.parseInt(intervalSplit[3]);
+								for (int i = Math.min(first, second); i <= Math.max(first, second); i++) {
+									result.add(i);
+								}
+								break;
+							}
+						} else {
+							result.add(Integer.parseInt(s));
+						}
+					}
+
+				}
+
+			}
+			break;
+		case CHAR:
+			if (Character.isDigit(getChar(option))) {
+				result.add(getChar(option) - '0');
+			}
+			break;
+		}
+
+		Collections.sort(result);
+		return result;
+	}
+
 	public int parse(String command_line_options) {
 		if (command_line_options == null) {
 			return -1;
@@ -101,7 +224,7 @@ public class Parser {
 		if (length == 0) {
 			return -2;
 		}
-		
+
 		int char_index = 0;
 		while (char_index < length) {
 			char current_char = command_line_options.charAt(char_index);
@@ -113,7 +236,7 @@ public class Parser {
 				}
 				char_index++;
 			}
-			
+
 			boolean isShortcut = true;
 			String option_name = "";
 			String option_value = "";
@@ -128,7 +251,7 @@ public class Parser {
 				return -3;
 			}
 			current_char = command_line_options.charAt(char_index);
-			
+
 			while (char_index < length) {
 				current_char = command_line_options.charAt(char_index);
 				if (Character.isLetterOrDigit(current_char) || current_char == '_') {
@@ -138,11 +261,11 @@ public class Parser {
 					break;
 				}
 			}
-			
+
 			boolean hasSpace = false;
 			if (current_char == ' ') {
 				hasSpace = true;
-				while (char_index < length) {				
+				while (char_index < length) {
 					current_char = command_line_options.charAt(char_index);
 					if (current_char != ' ') {
 						break;
@@ -155,7 +278,7 @@ public class Parser {
 				char_index++;
 				current_char = command_line_options.charAt(char_index);
 			}
-			if ((current_char == '-'  && hasSpace==true ) || char_index == length) {
+			if ((current_char == '-' && hasSpace == true) || char_index == length) {
 				if (getType(option_name) == BOOLEAN) {
 					option_value = "true";
 					if (isShortcut) {
@@ -184,7 +307,7 @@ public class Parser {
 					}
 				}
 			}
-			
+
 			if (isShortcut) {
 				optionMap.setValueWithOptioShortcut(option_name, option_value);
 			} else {
@@ -194,16 +317,15 @@ public class Parser {
 		}
 		return 0;
 	}
-	
+
 	private int getType(String option) {
 		int type = optionMap.getType(option);
 		return type;
 	}
-	
+
 	@Override
 	public String toString() {
 		return optionMap.toString();
 	}
 
 }
-
